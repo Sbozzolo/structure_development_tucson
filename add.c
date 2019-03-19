234198be5d40fb9361ef99e4d90bcd8a70cd385f
Gabriele Bozzola <sbozzolator@gmail.com>
2019-03-18 20:34:28 -0700

    Add add2

--- add.c
+++ add.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int add2(int a, int b){
+  return a + b;
+}
 
 int main(int argc, char *argv[])
 {
@@ -6,6 +11,10 @@ int main(int argc, char *argv[])
   if (argc != 3 && argc != 4){
     printf("Error!\n");
     return 1;
+  }
+
+  if (argc == 3){
+    printf("%d\n", add2(atoi(argv[1]), atoi(argv[2])));
   }
 
   return 0;
