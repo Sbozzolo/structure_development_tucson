2c435e84702cc005f629d7ae1e6c5a7d0e0ca1ae
Gabriele Bozzola <sbozzolator@gmail.com>
2019-03-18 20:37:16 -0700

    Add add3

--- add.c
+++ add.c
@@ -5,6 +5,10 @@ int add2(int a, int b){
   return a + b;
 }
 
+int add3(int a, int b, int c){
+  return add2(add2(a, b), c);
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -16,6 +20,11 @@ int main(int argc, char *argv[])
   if (argc == 3){
     printf("%d\n", add2(atoi(argv[1]), atoi(argv[2])));
   }
+
+  if (argc == 4){
+    printf("%d\n", add3(atoi(argv[1]), atoi(argv[2]), atoi(argv[3])));
+  }
+
 
   return 0;
 }
