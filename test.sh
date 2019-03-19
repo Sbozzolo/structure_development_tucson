d5db186699f2f0ef3d455f7275cef3f00982a1cd
Gabriele Bozzola <sbozzolator@gmail.com>
2019-03-18 19:30:17 -0700

    Add missing parens in test.sh that threw errors

--- test.sh
+++ test.sh
@@ -3,18 +3,24 @@
 PROGRAM_NAME="./add"
 
 # Test add 2 3, should be 5
-test1="$PROGRAM_NAME 2 3"
+test1=$("$PROGRAM_NAME" 2 3)
 if [ "$test1" -ne 5 ]
 then
    echo "Test 1 failed!"
    return 1
+else
+   echo "Test 1 passed!"
 fi
 
 
 # Test add 2 3 4, should be 9
-test2="$PROGRAM_NAME 2 3 4"
+test1=$("$PROGRAM_NAME" 2 3 4)
 if [ "$test1" -ne 9 ]
 then
    echo "Test 2 failed!"
    return 1
+else
+   echo "Test 2 passed!"
 fi
+
+return 0
