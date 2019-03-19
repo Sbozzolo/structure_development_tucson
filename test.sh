849b8d5caecb7a2c7364b9ce1ffa7ff3a1a350cc
Gabriele Bozzola <sbozzolator@gmail.com>
2019-03-18 20:02:26 -0700

    Make test positive and not negative

--- test.sh
+++ test.sh
@@ -2,25 +2,30 @@
 
 PROGRAM_NAME="./add"
 
+# What to output at the end
+return_value=1
+
 # Test add 2 3, should be 5
 test1=$("$PROGRAM_NAME" 2 3)
-if [ "$test1" -ne 5 ]
+if [ "$test1" -eq 5 ]
 then
+   echo "Test 1 passed!"
+   return_value=0
+else
    echo "Test 1 failed!"
    exit 1
-else
-   echo "Test 1 passed!"
 fi
 
 
 # Test add 2 3 4, should be 9
 test1=$("$PROGRAM_NAME" 2 3 4)
-if [ "$test2" -ne 9 ]
+if [ "$test2" -eq 9 ]
 then
+   echo "Test 2 passed!"
+   return_value=0
+else
    echo "Test 2 failed!"
    exit 1
-else
-   echo "Test 2 passed!"
 fi
 
-exit 0
+exit "$return_value"
